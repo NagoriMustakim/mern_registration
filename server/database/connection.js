c35c71ad0add096512f0270ac77a6f0aba9228e6
mustakim nagori <mustakimkhan1111176@gmail.com>
2023-06-27 18:42:08 +0530

    client done

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 const { MongoMemoryServer } = require('mongodb-memory-server')
-
+require('dotenv').config()
 async function connect() {
     const mongodb = new MongoMemoryServer()
     await mongodb.start()
     const mongo_URI = mongodb.getUri()
-    const db = await mongoose.connect(mongo_URI)
+    // const db = await mongoose.connect(mongo_URI)
+    const db = process.env.MONGODB_URI
     console.log("mongodb connected")
     return db
 
--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const app = express()
 const port = process.env.PORT || 4000
 
 app.use(express.json())
-app.use(cors())
-app.use('/api', router)
+app.use(cors()) 
+app.use('/api/v1', router)
 app.use(morgan('tiny'));
 app.disable('x-powered-by')
 
